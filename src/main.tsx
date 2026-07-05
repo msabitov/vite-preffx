9f40a04c1ec355d7e8bcc1502378d165b86ee639
msabitov <effnd@mail.ru>
2026-07-05 11:07:39 +0300

    v1.1.0, changes:
    1. PreffX version was updated.
    2. `root.mount` call was fixed.

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'preffx'
 import './index.css'
 import { App } from './App.tsx'
 
-const root = createRoot(document.getElementById('app'))
-root.mount(<App />)
+const root = createRoot(document.getElementById('app') as HTMLElement );
+root.mount(App)
